e90037914028414927ff8d6931a34ffbb074dd10
Thibaut Géry <thibaut.gery@gmail.com>
2015-11-05 18:09:09 +0100

    add # in URL

--- front/src/app.config.js
+++ front/src/app.config.js
@@ -1,6 +1,5 @@
 routing.$inject = ['$urlRouterProvider', '$locationProvider'];
 
 export default function routing($urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
 }
