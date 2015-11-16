0af27d9a845d663dcf4bd3d81ec49f618aa11c77
Thibaut Géry <thibaut.gery@gmail.com>
2015-11-16 14:45:29 +0100

    add essayez section

--- front/src/features/landingPage/index.js
+++ front/src/features/landingPage/index.js
@@ -5,7 +5,11 @@ import './css/main.css';
 import './css/landingPage.css';
 require('font-awesome/css/font-awesome.css');
 
-
+require('highlight.js/styles/github.css')
+var hljs = require('highlight.js/lib/highlight')
+hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
+hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+hljs.initHighlightingOnLoad();
 
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
