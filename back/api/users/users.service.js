e9c9c7ad812d03f58e98420ffa5681fa0a47cf7c
Thibaut Géry <thibaut.gery@gmail.com>
2016-02-26 11:41:41 +0100

    add users service

---
+++ back/api/users/users.service.js
@@ -0,0 +1,41 @@
+'use strict';
+const StandardError = require('standard-error');
+
+class UserService {
+
+  constructor(options) {
+    this.redis = options.redis.driver
+    this.key = options.redis.userPrefix;
+  }
+
+  getUser(email, callback) {
+    this.redis.get(this.key + '::' + email, (err, result) => {
+      if(err) {
+        return callback(new StandardError("Impossible to connect to redis", {code: 500}))
+      }
+      callback(null, JSON.parse(result))
+    })
+  }
+
+  setUser(user, callback) {
+    this.redis.set(this.key +'::'+ user.email, JSON.stringify(user), (err) => {
+      if(err) {
+        return callback(new StandardError("Impossible to connect to redis", {code: 500}))
+      }
+      callback(null, user)
+    })
+  }
+
+  deleteUser(email, callback) {
+    this.redis.del(this.key + '::'+ email, (err, result) => {
+      if(err) {
+        return callback(new StandardError("Impossible to connect to redis", {code: 500}))
+      }
+      callback(null, result)
+    })
+
+  }
+}
+
+
+module.exports = UserService
