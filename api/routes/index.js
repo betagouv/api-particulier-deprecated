48d407bc33c9cb7d3e32fb549b0a2da892c012e4
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-20 14:14:06 +0100

    Add API CAF

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,22 @@
-const caf = require('./../caf')
+const cafRouter = require('api-caf/lib/router')
 const impots = require('./../impots')
 const system = require('./../system')
 const analytics = require('./../analytics')
+const fs = require('fs')
 
 exports.configure = function (app, options) {
   app.use('/api', system(options));
 
   app.use('/api/impots', impots(options));
 
-  app.use('/api/caf', caf(options));
+  app.use('/api/caf', cafRouter({
+    serviceParams: {
+      host: options.cafHost,
+      cert: fs.readFileSync(options.cafSslCertificate),
+      key: fs.readFileSync(options.cafSslKey)
+    },
+    pingParams: options.cafPingParams
+  }));
 
   app.use('/api/analytics', analytics(options));
 };
