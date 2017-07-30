eca898dc9a35a547751f204c8274acbffd309764
Pascal Knoth <pknoth@octo.com>
2017-07-30 14:07:43 +0200

    Revert "Revert "log to a file""
    This reverts commit bf7d2f6740743fb7fae60af1d982b12aff48c710.

--- api/server.js
+++ api/server.js
@@ -33,7 +33,6 @@ function Server (options) {
   app.set('cafSslKey', options.cafSslKey)
   app.set('mongoDbUrl', options.mongoDbUrl)
   app.set('tokenService', options.tokenService)
-  app.set('franceConnectHost', options.franceConnectHost)
   app.set('banBaseUrl', options.ban.baseUrl)
   app.disable('x-powered-by')
   app.use(express.static('public'))
