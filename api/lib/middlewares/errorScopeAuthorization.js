32c3c98abaa993dacc8eaf9781f0f885b5550717
Pascal Knoth <pknoth@octo.com>
2018-03-09 16:06:42 +0100

    refactoring

--- api/lib/middlewares/errorScopeAuthorization.js
+++ api/lib/middlewares/errorScopeAuthorization.js
@@ -2,13 +2,8 @@ const StandardError = require('standard-error')
 
 module.exports = function (err, req, res, next) {
   if (err instanceof StandardError) {
-    if (err.scope === 'cnaf') {
-      if (!req.consumer.scopes.some((e) => e.match(/cnaf/))) {
-        return next(new StandardError('Your scopes are invalid. You are not authorized to access this resource.', {code: 403}))
-      }
-    }
-    if (err.scope === 'dgfip') {
-      if (!req.consumer.scopes.some((e) => e.match(/dgfip/))) {
+    if (err.scope && err.scope.match(/cnaf|dgfip/)) {
+      if (!req.consumer.scopes.some((e) => e.match(err.scope))) {
         return next(new StandardError('Your scopes are invalid. You are not authorized to access this resource.', {code: 403}))
       }
     }
