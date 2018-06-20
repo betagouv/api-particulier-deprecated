6703931e8296a4251fd11841dd689920e4d88b40
Pascal Knoth <pknoth@octo.com>
2018-06-20 14:40:08 +0200

    removes api/lib folder

--- api/lib/middlewares/identifyUser.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = function identifyUser (req, res, next) {
-  req.user = req.headers['X-User'] || req.query.user || 'Anonymous'
-  next()
-}
--- api/lib/utils/nconf.js
+++
@@ -1,8 +0,0 @@
-const nconf = require('nconf')
-
-nconf.env({
-  separator: '_'
-}).argv()
-nconf.defaults(require('../../../defaults'))
-
-module.exports = nconf
