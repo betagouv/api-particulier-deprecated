2b358c40e09d8e571413025a27547a8b8fb6432c
Stanislas Bernard <bernardstanislas@gmail.com>
2020-01-20 16:56:24 +0100

    :sparkles: Expose test API for student information

---
+++ api/student/student.controller.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const StandardError = require("standard-error");
+const axios = require("axios");
+
+class StudentController {
+  constructor(options) {
+    this.options = options || {};
+    this.baseUrl = options.supdataHost;
+    this.apiKey = options.supdataApiKey;
+    this.authorize = this.authorize.bind(this);
+    this.ping = this.ping.bind(this);
+    this.student = this.student.bind(this);
+    this.consumerMatch = this.consumerMatch.bind(this);
+  }
+
+  authorize(req, res, next) {
+    if (req.authType === "FranceConnect") {
+      if (this.consumerMatch(req, res)) {
+        return next();
+      } else {
+        return next(
+          new StandardError("You are forbidden to access this resource", {
+            code: 403
+          })
+        );
+      }
+    } else {
+      return next();
+    }
+  }
+
+  async ping(req, res, next) {
+    try {
+      await axios.get(`${this.baseUrl}/ping`);
+      return res.send("pong");
+    } catch (error) {
+      console.log(error);
+      return next(
+        new StandardError(error.message, { code: 500, scope: "etudiant" })
+      );
+    }
+  }
+
+  async student(req, res, next) {
+    var ine = req.query.ine;
+    if (!ine) {
+      return next(
+        new StandardError(
+          "Le paramètre ine doit être fourni dans la requête.",
+          { code: 400, scope: "etudiant" }
+        )
+      );
+    }
+    try {
+      const { data: student } = await axios.get(
+        `${this.baseUrl}/etudiantParIne`,
+        {
+          params: {
+            INE: ine
+          },
+          headers: {
+            "X-API-Key": this.apiKey
+          }
+        }
+      );
+      return res.json(student);
+    } catch (error) {
+      console.log(error);
+      return next(
+        new StandardError(error.message, {
+          code: error.response.status,
+          scope: "etudiant"
+        })
+      );
+    }
+  }
+
+  consumerMatch(req, res) {
+    return req.query.ine && req.query.ine === res.data.ine;
+  }
+}
+
+module.exports = StudentController;
