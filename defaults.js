35d73fe410f979ae95b18d30654c58d2dd0b2990
Stanislas Bernard <bernardstanislas@gmail.com>
2020-01-22 17:01:44 +0100

    :wrench: Replace IP with DNS

--- defaults.js
+++ defaults.js
@@ -7,7 +7,7 @@ module.exports = {
   franceConnectHost: 'fcp.integ01.dev-franceconnect.fr',
   cafHost: 'https://pep-test.caf.fr',
   svairHost: 'https://cfsmsp.impots.gouv.fr',
-  supdataHost: 'http://194.57.7.166/supdata/api/rest.php',
+  supdataHost: 'http://dev-supdata.renater.fr/supdata/api/rest.php',
   supdataApiKey: '3841b13fa8032ed3c31d160d3437a76a',
   cafSslCertificate: './cert/bourse.sgmap.fr.bundle.crt',
   cafSslKey: './cert/bourse.sgmap.fr.key',
